4a97b080bf00a8f8808653f29298ed8bde0c7554
Abigail Nielson <aa.nielson18@gmail.com>
2020-04-10 15:00:29 -0600

    added links to firebase. adjusted javascript so that new trains are added to firebase and then displayed in the table

--- assets/js/train.js
+++ assets/js/train.js
@@ -1,57 +1,83 @@
-// Initialize Firebase
-// Make sure to match the configuration to the script version number in the HTML
-// (Ex. 3.0 != 3.7.0)
-var config = {
-
-}
-firebase.initializeApp(config);
-var database = firebase.database();
-
-//Capture Button Click
-$("#add-train").on("click", function(event) {
-    //Capture user inputes and store them into variables
-    //console log each of the user inputs to confirm we are receiving them correctly
-    var name = $("#name-input").val().trim();
-    $("#name-display").html(name);
-
-    var destination = $("#destination-input").val().trim();
-    $("#destination-display").html(destination);
-
-    var firstTrain = $("#first-train-input").val().trim();
-    $("#first-train-display").html(DOMMatrixReadOnly);
-
-    var frequency = $("#frequency-input").val().trim();
-    $("#frequency-display").html(frequency);
-    
-    console.log(name);
-    console.log(destination);
-    console.log(firstTrain);
-    console.log(frequency);
-
-    //output all of the new information into the relevant HTML sections
-    var createRow = function(data) {
-        //create a new table row element
-        var tRow = $("<tr>");
-        // Methods run on jQuery selectors return the selector they we run on
-        // This is why we can create and save a reference to a td in the same statement we update its text
-        var trainName = $("<td>").text(name);
-        var trainDestination = $("<td>").text(destination);
-        var freqMin = $("<td>").text(frequency);
-        ///need to use math to adjust firstTrain variable into the next train
-        var nextTrain = $("<td>").text();
-        ///need to use math to adjust firstTrain variable/nextTrain var into the minutes away
-        var minAway = $("<td>").text();
-        //append the newly create table data to the rable row
-        tRow.append(trainName, trainDestination, freqMin, nextTrain, minAway);
-        //append the table row to the table body
-        $("tbody").append(tRow);
+$(document).ready(function(){
+    // Initialize Firebase
+    // Make sure to match the configuration to the script version number in the HTML
+    // (Ex. 3.0 != 3.7.0)
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDtW8hfELIfL4zUPrDKBSJMqBuoin4IW3E",
+        authDomain: "aanielson-695a2.firebaseapp.com",
+        databaseURL: "https://aanielson-695a2.firebaseio.com",
+        projectId: "aanielson-695a2",
+        storageBucket: "aanielson-695a2.appspot.com",
+        messagingSenderId: "643409531585",
+        appId: "1:643409531585:web:a89f79b86cdb386849fa40",
+        measurementId: "G-Y10051H55D"
     };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-    //push the variables to the database
-    database.ref().push({
-        name: name,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency,
-    })
+    // A variable to reference the database.
+    var database = firebase.database();
+
+    //create global variables
+    var name;
+    var destination;
+    var firstTrain;
+    var frequency = 0;
+
+    //Capture Button Click
+    $("#add-train").on("click", function(event) {
+        //Capture user inputes and store them into variables
+        //console log each of the user inputs to confirm we are receiving them correctly
+        name = $("#name-input").val().trim();
+        console.log(name);
+        
+        destination = $("#destination-input").val().trim();
+        console.log(destination);
+
+        firstTrain = $("#first-train-input").val().trim();
+        console.log(firstTrain);
+
+        frequency = $("#frequency-input").val().trim();
+        console.log(frequency);
+
+        //push the variables to the database
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $('form')[0].reset();
+    });
+
+    database.ref().on('child_added', function(childSnapshot) {
+        var firstTrainNew = moment(childSnapshot.val().firstTrain, 'hh:mm');
+        // Difference between the current and firstTrain
+        var diffTIme = moment().diff(moment(firstTrainNew), 'minutes');
+        var remainder = diffTIme % childSnapshot.val().frequency;
+        // Minutes until next train
+        var minAway = childSnapshot.val().frequency - remainder;
+        // Next train time
+        var nextTrain = moment().add(minAway, 'minutes');
+        
+        //output all of the new information into the relevant HTML sections
+        var createRow = function(data) {
+            //create a new table row element
+            var tRow = $("<tr>");
+            // Methods run on jQuery selectors return the selector they we run on
+            // This is why we can create and save a reference to a td in the same statement we update its text
+            var trainName = $("<td>").text(childSnapshot.val().name);
+            var trainDestination = $("<td>").text(childSnapshot.val().destination);
+            var freqMin = $("<td>").text(childSnapshot.val().frequency);
+            var nextTrainDisplay = $("<td>").text(moment(nextTrain).format('hh:mm'));
+            var minAwayDisplay = $("<td>").text(minAway);
+            //append the newly create table data to the rable row
+            tRow.append(trainName, trainDestination, freqMin, nextTrainDisplay, minAwayDisplay);
+            //append the table row to the table body
+            $("tbody").append(tRow);
+        };
+        createRow();
+    });
 });
